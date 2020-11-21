5f83be4513714ae995ed1338a3860e49f97f4093
Koo <theonlymoon04@gmail.com>
2020-11-21 18:46:45 +0900

    add update 커밋

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template, request
 from pymongo import MongoClient
 
+
+from flask import render_template, request, flash, redirect, url_for
+
+
+
 app = Flask(__name__)
 
 client = MongoClient('localhost', 27017) # mongoDB의 27017 포트로 들어감
-db = client.dbgongcha   # 'dbgongcha'라는 이름의 db 생성
+client = client.dbgongcha   # 'dbgongcha'라는 이름의 db 생성
+db = client['dbgongcha'] # 공처 db뭉치 가져오기
+menu_collection = db['menu'] #menu collection 불러오기
 
 
 ## HTML을 주는 부분
@@ -13,39 +20,32 @@ def home():
     return render_template('index.html')
 
 ## API 역할을 하는 부분
-@app.route('/memo', methods=['POST'])
-def post_article():
-    # 1. 클라이언트로부터 데이터를 받기
-    url_receive = request.form['url_give']
-    comment_receive = request.form['comment_give']
+@app.route('/menu/login')
+def manager_login():
+    pass
+    # return render_template('index.html', note=note)
 
-    # 2. meta tag를 스크래핑하기
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = requests.get(url_receive, headers=headers)
-    soup = BeautifulSoup(data.text, 'html.parser')
 
-    og_image = soup.select_one('meta[property="og:image"]')
-    og_title = soup.select_one('meta[property="og:title"]')
-    og_description = soup.select_one('meta[property="og:description"]')
+@app.route('/menu', methods=['POST', 'GET'])
+def show_menu():
+    menu = db.menu_collection.find()
+    return render_template('menu.html', menu = menu)
 
-    url_title = og_title['content']
-    url_description = og_description['content']
-    url_image = og_image['content']
+# 노트 생성
+@app.route('/menu/add', methods=['POST', 'GET'])
+def add_menu():
+    if request.method == 'POST':
+        db.menu_collection.insertOne({
+            # "_id" : "gdhong",
+            "카테고리" : request.form['category'],
+            "상품명" : request.form['title'],
+            "금액" : request.form['price']
+            })
+                    
+        return redirect(url_for('show_menu'))
 
-    article = {'url': url_receive, 'title': url_title, 'desc': url_description, 'image': url_image, 'comment': comment_receive}
+    return render_template('add.html')
 
-    # 3. mongoDB에 데이터 넣기
-    db.articles.insert_one(article)
-
-    return jsonify({'result': 'success'})
-
-@app.route('/memo', methods=['GET'])
-def read_articles():
-    # 1. mongoDB에서 _id 값을 제외한 모든 데이터 조회해오기(Read)
-    result = list(db.articles.find({}, {'_id': 0}))
-    # 2. articles라는 키 값으로 articles 정보 보내주기
-    return jsonify({'result': 'success', 'articles': result})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
