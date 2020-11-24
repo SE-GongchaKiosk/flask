5aa13668b72c48110bf59d4527cf5aa24e3cd05d
Koo <theonlymoon04@gmail.com>
2020-11-25 01:06:16 +0900

    카테고리 추가

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
-from flask import Flask, render_template, request
 from pymongo import MongoClient
-
-
-from flask import render_template, request, flash, redirect, url_for
+from flask import render_template, request, flash, redirect, url_for, Flask
 
 
 
@@ -10,8 +7,24 @@ app = Flask(__name__)
 
 client = MongoClient('localhost', 27017) # mongoDB의 27017 포트로 들어감
 client = client.dbgongcha   # 'dbgongcha'라는 이름의 db 생성
-db = client['dbgongcha'] # 공처 db뭉치 가져오기
-menu_collection = db['menu'] #menu collection 불러오기
+db = client.dbgongcha # 공처 db뭉치 가져오기
+
+
+# db.menu.insert_one({'number':1, 'category':'시즌메뉴', 'name':'토피넛 밀크티+펄(L)', 'price': 4900, 'ice':True, 'hot':True, 'topping_number':3, 'is_waiting':False, 'is_output_kitchen':True, 'img': None})
+# db.menu.insert_one({'number':2, 'category':'시즌메뉴', 'name':'초코바른 토피넛  스무디', 'price':5500, 'ice':True, 'hot':True, 'topping_number':3, 'is_waiting':False, 'is_output_kitchen':True, 'img': None})
+# db.menu.insert_one({'number':3, 'category':'베스트 콤비네이션', 'name':'블랙 밀크티+펄(L)', 'price':4500, 'ice':True, 'hot':True, 'topping_number':3, 'is_waiting':False, 'is_output_kitchen':True, 'img': None})
+# db.menu.insert_one({'number':4, 'category':'베스트 콤비네이션', 'name':'타로 밀크티+펄(L)', 'price':4500, 'ice':True, 'hot':True, 'topping_number':3, 'is_waiting':False, 'is_output_kitchen':True, 'img': None})
+# db.menu.insert_one({'number':5, 'category':'베스트 콤비네이션', 'name':'제주 그린 밀크티+펄(L)', 'price':4900, 'ice':True, 'hot':True, 'topping_number':3, 'is_waiting':False, 'is_output_kitchen':True, 'img': None})
+
+
+# 디비 내용 확인용
+all_menu = list(db.menu.find({}))
+for i in all_menu:
+    print(i)
+
+all_category = list(db.category.find({}))
+for i in all_category:
+    print(i)
 
 
 ## HTML을 주는 부분
@@ -28,23 +41,32 @@ def manager_login():
 
 @app.route('/menu', methods=['POST', 'GET'])
 def show_menu():
-    menu = db.menu_collection.find()
-    return render_template('menu.html', menu = menu)
+    all_menu = list(db.menu.find({}))
+    all_category = list(db.category.find({}))
+    return render_template('menu_change.html', all_menu = all_menu, all_category= all_category)
 
-# 노트 생성
-@app.route('/menu/add', methods=['POST', 'GET'])
-def add_menu():
+# 메뉴 생성
+@app.route('/menu/add_category', methods=['POST', 'GET'])
+def add_category():
     if request.method == 'POST':
-        db.menu_collection.insertOne({
-            # "_id" : "gdhong",
-            "카테고리" : request.form['category'],
-            "상품명" : request.form['title'],
-            "금액" : request.form['price']
-            })
-                    
+        category= request.form['add_category']
+        db.category.insert_one({'name': category} )
         return redirect(url_for('show_menu'))
+    else:
+        error = '잘못된 시도입니다'
+    return render_template('menu_change.html', error = error)
 
-    return render_template('add.html')
+# 메뉴 삭제
+@app.route('/menu/delete_category', methods=['POST'])
+def delete_category():
+    if request.method == 'POST':
+        category= request.form['delete_category']
+        db.category.delete_one({'name': category} )
+        return redirect(url_for('show_menu'))
+    else:
+        error = '잘못된 시도입니다'
+    return render_template('menu_change.html', error = error)
+    
 
 
 if __name__ == '__main__':
